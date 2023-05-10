9abd64285ec8879d1c7e2d81287e5f2a32e72aaf
WeronikaBabik <weronika.babik@outlook.com>
2023-05-10 19:26:43 +0000

    Deploying to gh-pages from @ WeronikaBabik/goit-js-hw-09@8134613cebcc19af5de6c5663a3905bb024fff99 🚀

---
+++ 01-color-switcher.8ec70ad3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=null,n=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(function(){d=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.8ec70ad3.js.map
