8134613cebcc19af5de6c5663a3905bb024fff99
VeranikaBabik <veranikababik@gmail.com>
2023-05-10 21:25:38 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timer = null;
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+stopBtn.disabled = true;
+startBtn.addEventListener('click', () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
