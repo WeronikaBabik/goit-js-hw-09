d47e5f9677c6a065a33e14588a99cef367d33d0b
VeranikaBabik <veranikababik@gmail.com>
2023-05-15 16:08:12 +0200

    edited step in task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,5 @@
 import Notiflix from 'notiflix';
+
 const inputDelay = document.querySelector("input[name='delay']");
 const inputStep = document.querySelector("input[name='step']");
 const inputAmount = document.querySelector("input[name='amount']");
@@ -20,10 +21,11 @@ inputBtn.addEventListener('click', submitFunction);
 
 function submitFunction(ev) {
   ev.preventDefault();
-  const amount = inputAmount.value;
-  let delay = inputDelay.value;
-  const step = inputStep.value;
+  let amount = Number(inputAmount.value);
+  let delay = Number(inputDelay.value);
+  let step = Number(inputStep.value);
   let newDelay = delay;
+
   for (let i = 1; i <= amount; i++) {
     createPromise(i, newDelay)
       .then(({ position, delay }) => {
@@ -36,6 +38,6 @@ function submitFunction(ev) {
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
-    delay = delay + i * step;
+    newDelay = delay + i * step;
   }
 }
